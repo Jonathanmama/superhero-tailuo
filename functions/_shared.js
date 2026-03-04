ddf4339b20264dc46a4c847d4c35d62e4884b37d
JonathanMazy <77202020+JonathanMazy@users.noreply.github.com>
2026-03-04 21:09:49 +0800

    fix: reduce pbkdf2 iterations for cloudflare workers

--- functions/_shared.js
+++ functions/_shared.js
@@ -47,7 +47,7 @@ export async function hashPassword(password, saltHex) {
     {
       name: 'PBKDF2',
       salt: hexToBytes(saltHex),
-      iterations: 120000,
+      iterations: 100000,
       hash: 'SHA-512'
     },
     key,
